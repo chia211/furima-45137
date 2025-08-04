858467a28a938ee9c367d29a779efc7a477635e1
chia211 <sg2411019999@gmail.com>
2025-08-04 18:47:50 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
-  belong_to :user
+  belongs_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  validates :name, :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
