7045f25f0a2958c10f5066dbd171c1b1e3bb0157
chia211 <sg2411019999@gmail.com>
2025-08-06 16:56:31 +0900

    items#create

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,18 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :shipping_day
-  validates :name, :description, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :image
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  end
+  with_options numericality: { other_than: 1, message: "を選択してください" } do
+    validates :category_id
+    validates :quality_id
+    validates :shipping_day_id
+    validates :prefecture_id
+    validates :postage_id
+  end
 end
