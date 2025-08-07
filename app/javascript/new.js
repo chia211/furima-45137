9bdc7f1b02676cf08355999bda7e118fc6eb8ccd
chia211 <sg2411019999@gmail.com>
2025-08-07 20:32:33 +0900

    商品出品機能　レビュー後修正２

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -5,7 +5,7 @@ const price = () => {
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue * 0.9)
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML)
 
   });
 
