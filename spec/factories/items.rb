f1403e175d8feb0f5bf21f45e5e8bb7c51f79988
chia211 <sg2411019999@gmail.com>
2025-08-07 17:31:57 +0900

    item 単体テストコード雛形

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name             { '商品名サンプル' }
+    description      { 'これはテスト用の説明文です。1000文字以内で記述。'}
+    category_id      { 2 }
+    quality_id       { 2 }
+    postage_id       { 2 }
+    prefecture_id    { 2 }
+    shipping_day_id  { 2 }
+    price            { Faker::Number.between(from: 300, to: 9_999_999) }
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '出品できる場合' do
+      it "nameとdescriptionとcategory_idとquality_idとpostage_idとprefecture_idとshipping_day_idとpriceとimageが存在すれば登録できる" do
+        
+      end
+    end
+    context '出品できない場合' do
+      it "nameが空では登録できない" do
+        
+      end
+      it "nameが40文字を超えていたら登録できない" do
+
+      end
+      it "descriptionが空では登録できない" do
+
+      end
+      it "descriptionが1000文字を超えていたら登録できない" do
+
+      end
+      it "category_idが空では登録できない" do
+
+      end
+      it "quality_idが空では登録できない" do
+
+      end
+      it "postage_idが空では登録できない" do
+
+      end
+      it "prefecture_idが空では登録できない" do
+
+      end
+      it "shipping_day_idが空では登録できない" do
+
+      end
+      it "priceが空では登録できない" do
+
+      end
+      it "priceが半角数字でなかったら登録できない" do
+
+      end
+      it "priceが300円未満では登録できない" do
+
+      end
+      it "priceが9_999_999円を超えていたら登録できない" do
+
+      end
+      it "imageが空では登録できない" do
+
+      end
+    end
+  end
 end
