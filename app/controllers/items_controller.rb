6982ef100f3e7ca2ab4caeeed0737e97bb18acd8
chia211 <sg2411019999@gmail.com>
2025-08-10 18:53:32 +0900

    orderモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   private
 
   def item_params
