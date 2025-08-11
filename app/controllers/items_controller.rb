2437ea4a8373e245bf96de3d984cda33b03abd5a
chia211 <sg2411019999@gmail.com>
2025-08-11 20:34:06 +0900

    エラーハンドリングができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     if @item.user_id != current_user.id
-      redirect_to  user_session_path
+      redirect_to  root_path
     else
       render :edit
     end
