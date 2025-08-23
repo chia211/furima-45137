5d15c45dc81217dcb69eb8ca1af16334707a88bf
chia211 <sg2411019999@gmail.com>
2025-08-23 14:25:06 +0900

    クレカ入力欄

---
+++ app/javascript/card.js
@@ -0,0 +1,24 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_d0fc2f3e3a1a9011e2241b70')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+  const form = document.getElementById('button')
+  form.addEventListener("click", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+    e.preventDefault();
+  });
+};
+
+window.addEventListener("turbo:load", pay);
